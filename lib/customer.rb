afa5c7048e0636c9bf89c371dfa73dc50e97f31f
Emily Peri <emilyperi2@gmail.com>
2016-01-29 19:16:18 -0800

    working class method find_by_name and all

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,32 @@
 class Customer
 	attr_reader :name
 	
+	@@customers = []
+	
 	def initialize(op={})
 		@name = op[:name]
+		add_customer
 	end
+
+	def self.all
+    @@customers
+  end
+
+  def self.find_by_name(input_name)
+    customer = @@customers.find_index { |customer| customer.name == input_name }
+    if customer
+      @@customers[customer]
+    end
+  end
+
+	private
+
+	def add_customer
+		if @@customers.any? { |customer| customer.name == @name }
+			raise DuplicateCustomerError, "'#{@name}' already exists."
+		else
+			@@customers << self
+		end
+	end
+
 end
