bca07a8d52e1939dfdca82e8951f638999fb593a
Emily Peri <emilyperi2@gmail.com>
2016-01-29 19:29:19 -0800

    added method to Transaction for creating and assigning ids

--- app.rb
+++ app.rb
@@ -47,12 +47,12 @@ puts Product.all.count # Should return 3
 
 # TRANSACTIONS
 
-# transaction = Transaction.new(walter, nanoblock)
+ transaction = Transaction.new(walter, nanoblock)
 
-# puts transaction.id # Should return 1
-# puts transaction.product == nanoblock # Should return true
-# puts transaction.product == firehouse # Should return false
-# puts transaction.customer == walter # Should return true
+ puts transaction.id # Should return 1
+ puts transaction.product == nanoblock # Should return true
+ puts transaction.product == firehouse # Should return false
+ puts transaction.customer == walter # Should return true
 
 # puts nanoblock.stock # Should return 11
 
--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,8 +1,22 @@
 class Transaction
-	attr_reader :customer, :product
+	attr_reader :customer, :product, :id
+
+	@@transactions = []
+	@@id_count = 0
 
 	def initialize(customer, product)
 		@customer = customer
 		@product = product
+		@id = new_id
+	 	@@transactions << self
 	end
+
+	private
+
+	def new_id
+		@@id_count += 1
+		@@id_count
+	end
+
+
 end
